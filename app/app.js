99bad548b240772fc8e4a8ededb65dfcf70a9262
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-03 22:35:04 +0530

    Cleaned up app.js by having all the configuration code within a configure() method. This method is then called from www script.

--- app/app.js
+++ app/app.js
@@ -9,54 +9,58 @@ var swig = require('swig');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+/**
+ * Express app initialization and configuration
+ */
 var app = express();
 
-// view engine setup
-app.engine('html', swig.renderFile);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.configure = function () {
+    // view engine setup
+    app.engine('html', swig.renderFile);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(__dirname + '/public/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+    app.use('/', routes);
+    app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-// error handlers
+    // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: {}
         });
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+};
 
 module.exports = app;
