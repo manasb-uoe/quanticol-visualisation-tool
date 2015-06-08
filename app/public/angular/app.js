08efaa786f10abfb1abe9b0b997afd0b9acb4568
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-08 17:11:54 +0530

    Implemented navigation between the tool and documentation views using the navigation bar.

--- app/public/angular/app.js
+++ app/public/angular/app.js
@@ -21,8 +21,8 @@ app.config([
             templateUrl: "/angular/views/tool.html"
         });
 
-        $stateProvider.state("about", {
-            url: "/about",
+        $stateProvider.state("documentation", {
+            url: "/documentation",
             templateUrl: "/angular/views/about.html"
         });
 
@@ -36,16 +36,30 @@ app.config([
  * Controllers
  */
 
-app.controller("ToolController", [
+app.controller("NavigationController", [
     "$scope",
     function ($scope) {
-        $scope.title = "Tool";
-    }
-]);
+        $scope.tabs = [
+            {name: "Tool", active: true, state: "tool", iconClass: "glyphicon-wrench"},
+            {name: "Docmentation", active: false, state: "documentation", iconClass: "glyphicon-question-sign"}
+        ];
 
-app.controller("AboutController", [
-    "$scope",
-    function ($scope) {
-        $scope.title = "About";
+        $scope.selectTab = function (selectedTab) {
+            for (var i=0; i<$scope.tabs.length; i++) {
+                if (!$scope.tabs[i] != selectedTab) {
+                    $scope.tabs[i].active = false;
+                }
+            }
+            selectedTab.active = true;
+        };
+
+        $scope.$on("$locationChangeSuccess", function (event, newState) {
+            newState = newState.substring(newState.indexOf("#/")+2, newState.length);
+            for (var i=0; i<$scope.tabs.length; i++) {
+                if ($scope.tabs[i].state == newState) {
+                    $scope.selectTab($scope.tabs[i]);
+                }
+            }
+        });
     }
 ]);
