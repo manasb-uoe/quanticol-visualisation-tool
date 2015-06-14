8d2b28b7e087bd65dc6c88f849e7c1a6aa298a40
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-14 11:56:06 +0530

    Added a control panel to Tool page. It can now be toggled via the control panel button which is always visible.

--- app/public/javascripts/views/tool.js
+++ app/public/javascripts/views/tool.js
@@ -14,10 +14,52 @@ define([
     var ToolView = Backbone.View.extend({
         el: "#content",
         initialize: function() {
+
+        },
+        events: {
+            "click .control-panel-trigger": "toggleControlPanel"
         },
         render: function () {
             var compiledTemplate = swig.render(toolTemplate);
             this.$el.html(compiledTemplate);
+
+            this.isControlPanelVisible = false;
+        },
+        toggleControlPanel: function () {
+            var $controlPanel = $(".control-panel");
+            var $controlPanelTriggerWrapper = $(".control-panel-trigger-wrapper");
+
+            if (this.isControlPanelVisible) {
+                $controlPanel.animate(
+                    {marginTop: "-250px"},
+                    {duration: 300, queue: false}
+                );
+                $controlPanelTriggerWrapper.animate(
+                    {marginTop: "0"},
+                    {duration: 300, queue: false}
+                );
+
+                $controlPanelTriggerWrapper.find(".glyphicon")
+                    .removeClass()
+                    .addClass("glyphicon glyphicon-chevron-down");
+
+                this.isControlPanelVisible = false;
+            } else {
+                $controlPanel.animate(
+                    {marginTop: "0"},
+                    {duration: 300, queue: false}
+                );
+                $controlPanelTriggerWrapper.animate(
+                    {marginTop: "250px"},
+                    {duration: 300, queue: false}
+                );
+
+                $controlPanelTriggerWrapper.find(".glyphicon")
+                    .removeClass()
+                    .addClass("glyphicon glyphicon-chevron-up");
+
+                this.isControlPanelVisible = true;
+            }
         }
     });
 
