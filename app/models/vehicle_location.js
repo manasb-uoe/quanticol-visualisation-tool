bf5bd041c333016f0dd3ad1d7084bd8e910d8f61
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-16 23:34:49 +0530

    Updated vehicles/ route to accept a filter parameter. Now it can return all possible vehicles for a given query, or just the unique vehicles.

--- app/models/vehicle_location.js
+++ app/models/vehicle_location.js
@@ -6,7 +6,7 @@ var mongoose = require("mongoose");
 
 var vehicleLocationSchema = new mongoose.Schema({
     vehicle_id: {type: String, index: true},
-    last_gps_fix: Date,
+    last_gps_fix: Number,
     location: {type: [Number], index: '2d'},
     speed: Number, // The speed (in miles per hour) of the vehicle
     heading: Number, // The heading (in degrees, 0-360) of the vehicle
--- app/routes/api.js
+++ app/routes/api.js
@@ -6,6 +6,7 @@ var express = require('express');
 var router = express.Router();
 var Service = require("../models/service");
 var VehicleLocation = require("../models/vehicle_location");
+var async = require("async");
 
 router.get("/services", function (req, res, next) {
     Service
@@ -16,17 +17,60 @@ router.get("/services", function (req, res, next) {
         });
 });
 
-router.get("/vehicles", function (req, res, next) {
+router.get("/vehicles/:filter", function (req, res, next) {
+    var filter = req.params.filter;
     var selectedServices = req.query["service"] || [];
+    var selectedVehicles = req.query["vehicle"] || [];
+    var startTime = req.query["startTime"] ? req.query["startTime"] : 0;
+    var endTime = req.query["endTime"] ? req.query["endTime"] : ((new Date()).getTime() / 1000).toFixed(0);
 
-    VehicleLocation
-        .where("service_name")
-        .in(selectedServices)
-        .exec(function (err, vehicles) {
-            if (err) return next(err);
+    switch (filter) {
+        case "unique":
+            VehicleLocation
+                .where("service_name")
+                .in(selectedServices)
+                .distinct("vehicle_id")
+                .exec(function (err, vehicleIDs) {
+                    if (err) return next(err);
 
-            res.json(vehicles);
-        });
+                    // now that we have the distinct vehicle ids, we will find one vehicle for each of these ids
+                    var uniqueVehicles = [];
+                    async.each(
+                        vehicleIDs,
+                        function (vehicleID, cb) {
+                            VehicleLocation.findOne({vehicle_id: vehicleID}, function (err, vehicle) {
+                                uniqueVehicles.push(vehicle);
+                                cb();
+                            });
+                        },
+                        function () {
+                            res.json(uniqueVehicles);
+                        }
+                    )
+                });
+            break;
+
+        case "all":
+            VehicleLocation
+                .where({
+                    service_name: {$in: selectedServices},
+                    vehicle_id: {$in: selectedVehicles},
+                    last_gps_fix: {
+                        $gte: startTime,
+                        $lte: endTime
+                    }
+                })
+                .exec(function (err, vehicles) {
+                    if (err) return next(err);
+
+                    res.json(vehicles);
+                });
+            break;
+
+        default:
+            return next(new Error("Filter can only be 'unique' or 'all'."));
+            break;
+    }
 });
 
 
