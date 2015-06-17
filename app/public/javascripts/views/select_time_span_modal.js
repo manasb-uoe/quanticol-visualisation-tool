7522bab09b203d6fed9a3458e8b4981c5e11c155
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-17 21:28:38 +0530

    Fixed wrong time format in datetimepicker.

--- app/public/javascripts/views/select_time_span_modal.js
+++ app/public/javascripts/views/select_time_span_modal.js
@@ -31,8 +31,8 @@ define([
             this.startTimePicker = $("#start-time-picker");
             this.endTimePicker = $("#end-time-picker");
 
-            this.startTimePicker.datetimepicker({locale: "en", format: "DD/MM/YYYY hh:mm", defaultDate: new Date("January 01, 2015 12:00")});
-            this.endTimePicker.datetimepicker({locale: "en", format: "DD/MM/YYYY hh:mm", defaultDate: new Date()});
+            this.startTimePicker.datetimepicker({locale: "en", format: "MMMM Do YYYY, h:mm a", defaultDate: new Date("January 01, 2015 12:00")});
+            this.endTimePicker.datetimepicker({locale: "en", format: "MMMM Do YYYY, h:mm a", defaultDate: new Date()});
 
             this.startTimePicker.on("dp.change", function (e) {
                 self.endTimePicker.data("DateTimePicker").minDate(e.date);
