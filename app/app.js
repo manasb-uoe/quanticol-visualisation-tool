5044ddbb3a08742dbabf6957f859a9b6e5c53b15
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-18 09:47:50 +0530

    Added bus favicon.

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ app.configure = function () {
     app.set('view engine', 'html');
 
     // uncomment after placing your favicon in /public
-    //app.use(favicon(__dirname + '/public/favicon.ico'));
+    app.use(favicon(__dirname + '/public/images/favicon.ico'));
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
