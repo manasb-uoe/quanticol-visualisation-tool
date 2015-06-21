d2b05565b0c6fe5dca3068b420b36576bb3b0022
Manas Bajaj <manas.bajaj94@gmail.com>
2015-06-21 22:08:21 +0530

    Added ability to view service description by right clicking on it in 'Select services' modal. Also made minor changes it the modal template.

--- app/public/javascripts/views/service_item.js
+++ app/public/javascripts/views/service_item.js
@@ -6,17 +6,19 @@ define([
     "jquery",
     "underscore",
     "backbone",
+    "bootstrap",
     "swig",
     "text!../../templates/service_item.html"
-], function($, _, Backbone, swig, serviceItemTemplate){
+], function($, _, Backbone, bootstrap, swig, serviceItemTemplate){
     "use strict";
 
     var ServiceItemView = Backbone.View.extend({
         initialize: function () {
-
+            this.model.on("change:isSelected", this.render, this);
         },
         events: {
-            "click .service": "toggleSelection"
+            "click .service": "toggleSelection",
+            "contextmenu .service": "togglePopover"
         },
         render: function () {
             var compiledTemplate = swig.render(serviceItemTemplate, {locals: this.model.toJSON()});
@@ -26,7 +28,14 @@ define([
         },
         toggleSelection: function() {
             this.model.set("isSelected", !this.model.get("isSelected"));
-            this.render();
+        },
+        togglePopover: function (event) {
+            event.preventDefault();
+            this.$el.find(".service").popover({
+                html: true,
+                content: "<strong>Description: </strong>" + this.model.get("description"),
+                placement: "top"
+            }).popover("toggle");
         }
     });
 
